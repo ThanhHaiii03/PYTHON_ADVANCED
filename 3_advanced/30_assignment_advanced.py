bb4683ef896db194531d900ded84f2362c353629
ThanhHai <nhai72085@gmail.com>
2025-11-04 20:47:45 +0700

    INIT_3_Advanced

---
+++ 3_advanced/30_assignment_advanced.py
@@ -0,0 +1,43 @@
+# ADVANCED **************************************************************************
+# content = assignment (Python Advanced)
+#
+# deliver = Upload the files to your git repository
+#           and share it in the assignment submission form.
+#
+# date    = 2025-03-18
+# email   = contact@alexanderrichtertd.com
+#*******************************************************************************
+"""
+
+Advanced Python is about a higher and more structural level of Python.
+Learning new functions, constructs and a better way of collaboration.
+
+
+
+#*******************************************************************************
+# 31. CUBE CLASS
+#*******************************************************************************
+Create a cube class in cube.py
+
+
+
+
+
+
+
+#*******************************************************************************
+# 32. DECORATOR
+#*******************************************************************************
+   a) Create a decorator in decorator.py
+
+   b) Include a decorator into your own application.
+
+
+
+
+
+
+#*******************************************************************************
+# 31_assignment_app.py
+#*******************************************************************************
+"""
---
+++ 3_advanced/31_assignment_app.py
@@ -0,0 +1,44 @@
+# ADVANCED **************************************************************************
+# content = assignment (Python Advanced)
+#
+# deliver = Upload the files to your git repository
+#           and share it in the assignment submission form.
+#
+# date    = 2025-03-18
+# email   = contact@alexanderrichtertd.com
+#*******************************************************************************
+
+"""
+#*******************************************************************************
+# 33. CLASSES & PARENTING
+#*******************************************************************************
+
+   a) Commit the "old" application version as "INIT 3_advanced"
+
+   b) Update one of your modules using classes.
+      Make sure to use one of the benefits of classes (else it's pointless).
+      Write a comment in the docString of the class why a class would improve this module.
+
+   c) OPTIONAL: Update one of your modules using class parenting.
+      Write a comment in the docString of the class why parenting would improve this module.
+
+      NOTE: b) & c) can be combined.
+
+
+
+
+
+
+
+#*******************************************************************************
+# 34. CONFIGURATION FILES
+#*******************************************************************************
+
+   a) Update one of your modules using configuration or data files.
+
+   b) Write a comment in the call of the configuration file
+      why a configuration or data file would improve this module.
+
+   c) Commit as "DONE 3_advanced" and push the updated version to "main/master".
+
+"""
